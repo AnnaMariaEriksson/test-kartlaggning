bd26e30bce2afc5a741fca2899b9839cbda8d051
Anna-Maria Eriksson <ameriksson.se@icloud.com>
2025-05-06 14:31:53 +0200

    Done with first two tasks

---
+++ introduktion/Variables/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Variables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        int x;
+        int y;
+
+        x = 7;
+        y = x + 3;
+        
+        Console.WriteLine(y);
+        */
+        
+        Console.WriteLine("What is your name?");
+        Console.Write("Type your name: ");
+        string firstName;
+        firstName = Console.ReadLine();
+
+        /*
+        string lastName;
+        Console.Write("Type your last name: ");
+        lastName = Console.ReadLine();*/
+        
+        Console.WriteLine("Type your last name: ");
+        string lastName = Console.ReadLine();
+        
+        Console.WriteLine("Hello, " + firstName + " " + lastName);
+        Console.ReadLine();
+    }
+}
---
+++ introduktion/introduktion/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace introduktion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine(); //wait for command
+        }
+    }
+}
+
+
