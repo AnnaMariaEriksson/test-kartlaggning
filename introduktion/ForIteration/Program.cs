009d2368fb7e032af6a3cca9cc19905dcdd86655
Anna-Maria Eriksson <ameriksson.se@icloud.com>
2025-05-06 16:26:13 +0200

    Done with the first 9 tasks

---
+++ introduktion/ForIteration/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ForIteration;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            //Console.WriteLine(i);
+            if (i == 7)
+            {
+                Console.WriteLine("Found seven!");
+                break; //break the iteration
+            }
+        }
+        
+        
+
+        Console.ReadLine();
+        
+    }
+}
---
+++ introduktion/SimpleMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleMethod;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       HelloWorld();
+       Console.ReadLine();
+    }
+
+    private static void HelloWorld()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
