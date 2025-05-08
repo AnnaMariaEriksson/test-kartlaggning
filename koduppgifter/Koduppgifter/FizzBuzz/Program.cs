03c8031e48c0f9a6fef3a6c2468466eb70683d36
Anna-Maria Eriksson <ameriksson.se@icloud.com>
2025-05-08 10:43:38 +0200

    Completed FizzBuzz task with Unit test

---
+++ koduppgifter/Koduppgifter/FizzBuzz/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace FizzBuzz;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        var inputSplit = input.Split(" ");
+        int x = int.Parse(inputSplit[0]);
+        int y = int.Parse(inputSplit[1]);
+        int n = int.Parse(inputSplit[2]);
+       string fbList = FizzBuzzResult(x, y, n);
+       Console.WriteLine(fbList);
+    }
+
+    public static string FizzBuzzResult(int x, int y, int n)
+    {
+        List<string> result = new List<string>();
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % x == 0 && i % y == 0)
+            {
+                result.Add("FizzBuzz");
+            }
+            else if (i % y == 0)
+            {
+                result.Add("Buzz");
+            }
+            else if (i % x == 0)
+            {
+                result.Add("Fizz");
+            }
+            else
+            {
+                result.Add(i.ToString());
+            }
+        }
+
+        return String.Join("\n", result.ToArray());
+    }
+}
---
+++ koduppgifter/Koduppgifter/Tests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using FizzBuzz;
+using NUnit.Framework;
+
+namespace Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void Test237()
+    {
+        
+        string result = Program.FizzBuzzResult(2, 3, 7);
+        Assert.That(result, 
+            Is.EqualTo("1\nFizz\nBuzz\nFizz\n5\nFizzBuzz\n7"));
+    }
+
+    [Test]
+    public void Test247()
+    {
+        string result = Program.FizzBuzzResult(2, 4, 7);
+        Assert.That(result, Is.EqualTo("1\nFizz\n3\nFizzBuzz\n5\nFizz\n7"));
+    }
+
+    [Test]
+    public void Test357()
+    {
+        string result = Program.FizzBuzzResult(3, 5, 7);
+        Assert.That(result, Is.EqualTo("1\n2\nFizz\n4\nBuzz\nFizz\n7"));
+    }
+}
