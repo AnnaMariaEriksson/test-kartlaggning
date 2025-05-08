1232ecfd6593c6323874ddc02c071f116ec13c9b
Anna-Maria Eriksson <ameriksson.se@icloud.com>
2025-05-08 21:57:53 +0200

    Added a helper class for repetitive code

---
+++ koduppgifter/Koduppgifter/HelperMethods/Helpers.cs
@@ -0,0 +1,17 @@
+﻿namespace HelperMethods;
+
+public class Helpers
+{
+    public static List<int> ParseStringsToInts(string numbers)
+    {
+        List<int> parsedInts = new List<int>();
+        string[] numberToBeParsed = numbers.Split(" ");
+
+        foreach (string s in numberToBeParsed)
+        {
+             parsedInts.Add(int.Parse(s));
+        }
+
+        return parsedInts;
+    }
+}
